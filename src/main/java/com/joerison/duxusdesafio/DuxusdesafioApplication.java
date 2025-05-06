f68f1701b103d6b5ca238902199fd0cc373c4b35
Joerison Silva <joerison@gmail.com>
2025-05-06 13:34:46 -0300

    first commit

---
+++ src/main/java/com/joerison/duxusdesafio/DuxusdesafioApplication.java
@@ -0,0 +1,13 @@
+package com.joerison.duxusdesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuxusdesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuxusdesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joerison/duxusdesafio/DuxusdesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joerison.duxusdesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuxusdesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
